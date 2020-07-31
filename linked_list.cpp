26e778cbae560b2ed98e0473958d128c6839bcee
Robert Baindourov <rbaindourov@gmail.com>
2020-07-31 12:54:06 -0700

    linked list - saving progress

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
+
 
 using namespace std;
 
-//come to me - kneel down for me - one more time
+//come to me - kneel down for me - one more time - c++98
+template <class Object> class List;
+
+class BadIterator : public logic_error {
+    public:
+        BadIterator( const string& msg = "") : logic_error( msg.c_str() ){}
+};
+
 template <class Object> class Node {
     public:
         Node ( const Object& el = Object(), Node* node = NULL ){
-
+            element = el;
+            next = node;
         }
-        const Object& getElement();
-        Node* getNext();
-        void setNext(Node* node);
+
+        const Object& getElement(){
+            return element;
+        }
+
+        Node* getNext(){
+            return next;
+        }
+
+        void setNext(Node* node){
+            next = node;
+        }
     private:
         Object element;
         Node* next;
@@ -21,9 +40,44 @@ template <class Object> class Node {
 
 
 
+template <class Object> class Iterator{
+    public:
+        Iterator() : current( NULL ) {};
+        bool isValid() const { return ( current != NULL ); };
+        void advance(){
+            if(isValid())
+                current = current->getNext();
+        };
+        const Object& retrieve() const{
 
+        }
+    private:
+        Node<Object>* current;
+        Iterator ( Node<Object> *node ) : current( node ) {}
+        friend class List<Object>;
+
+};
+
+
+template <class Object> class List{
+    public:
+        List();
+        List( const List& rhs );
+        ~List();
+        bool isEmpty() const;
+        void makeEmpty();
+        Iterator<Object> zeroth() const;
+
+        
+};
+
+
+//we are one
 int main()
 {
+
+
+    
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
     for (const string& word : msg)
