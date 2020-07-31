b5bdf21ce7241001f39e5bb342c4df653de9b2f7
Robert Baindourov <rbaindourov@gmail.com>
2020-07-31 10:30:08 -0700

    my kingdoms rising - i am the emperor of the sun

--- linked_list.cpp
+++ linked_list.cpp
@@ -4,6 +4,24 @@
 
 using namespace std;
 
+//come to me - kneel down for me - one more time
+template <class Object> class Node {
+    public:
+        Node ( const Object& el = Object(), Node* node = NULL ){
+
+        }
+        const Object& getElement();
+        Node* getNext();
+        void setNext(Node* node);
+    private:
+        Object element;
+        Node* next;
+};
+
+
+
+
+
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
