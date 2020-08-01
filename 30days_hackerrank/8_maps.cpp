94c5aa85a4300de02c0ccbb3a7a7271be5f23d6d
Robert Baindourov <robert.baindourov@mindtree.com>
2020-07-31 21:38:09 -0700

    day 8 solved

---
+++ 30days_hackerrank/8_maps.cpp
@@ -0,0 +1,69 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+vector<string> split_string(string);
+
+
+
+int main()
+{
+    int n;
+    map<string, string> m;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for( int lines =0; lines < n; lines++){
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+        vector<string> arr_temp = split_string(arr_temp_temp);
+        m.insert({arr_temp[0], arr_temp[1]});
+    }
+    
+
+    string queries;
+    while(getline(cin, queries) ){
+        if( m.find(queries) != m.end() ){
+            cout<<queries<<"="<<m[queries]<<endl;
+        } else {
+            cout<<"Not found"<<endl;
+        }
+    }
+
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
