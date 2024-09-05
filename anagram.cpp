f92f4f113b483741496e9d5fb08eb9f32d7fdd80
Robert Baindourov <rbaindourov@gmail.com>
2024-09-05 14:16:13 -0700

    more interview practice

--- anagram.cpp
+++ anagram.cpp
@@ -5,26 +5,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//etrade question
+
 int main()
 {
     
-    map<string,string> anagrams;
-    vector<string> msg { "black", "blue", "elbow", "below"};
+    map<string,vector<string>> anagrams;
+    vector<string> msg = { "black", "blue", "elbow", "below", "bake", "kabe", "keep", "peek", "knee", "kene","neek"};
     for (string& word : msg){
         const string copied_word = word;
         sort(word.begin(), word.end());
         if( anagrams.find(word) !=anagrams.end() ){
-            anagrams.insert({word,copied_word});
+            anagrams[word].push_back(copied_word);
         }else{
-            
+            anagrams[word] = {copied_word}; 
         };
     };
 
-    
+    for(auto& anagram : anagrams){
+        cout << anagram.first << " : ";
+        for(auto& word : anagram.second){
+            cout << word << " ";
+        }
+        cout << endl;
+    }
 
+    for(auto& anagram : anagrams){  
+        if(anagram.second.size() > 1){
+            cout << "Anagram group: ";
+            for(auto& word : anagram.second){
+                cout << word << " ";
+            }
+            cout << endl;
+        }
+    }   
 
+    return 0;
 }
-
-
-
