1004f4144b384400a90456379171bfaf24bdfb25
Robert Baindourov <rbaindourov@gmail.com>
2024-09-05 23:04:29 -0700

    clean up

--- ListNode.java
+++ ListNode.java
@@ -1,6 +1,18 @@
- Definition for singly-linked list.
+/* You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
  
-  class ListNode {
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+  
+*/
+
+class ListNode {
       int val;
       ListNode next;
       ListNode() {}
@@ -11,8 +23,8 @@
 
 
 
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+class ListNodeSolution {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
         int carry = 0;
@@ -33,8 +45,13 @@ class Solution {
         }
         return dummy.next;  
      
+    }
 
-
-
+    //generate test cases for the above function
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode result = addTwoNumbers(l1, l2);
+        System.out.println(result);
     }
 }
